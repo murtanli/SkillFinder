c4da634371e654800e8429016b4fa58582fe045f
murtanli <70170580+murtanli@users.noreply.github.com>
2023-04-10 16:26:49 +0300

    Vr 1.1.3

--- skillfinder/works/models.py
+++ skillfinder/works/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class users(models.Model):
+    login = models.CharField(max_length=15)
+    Password = models.CharField(max_length=10)
+
+class profile(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    surname = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(users,on_delete=models.PROTECT)
+
+class worker(models.Model):
+    profile_wor = models.ForeignKey(profile, on_delete=models.PROTECT, db_index=True)
+
+class customer(models.Model):
+    profile_cus = models.ForeignKey(profile, on_delete=models.PROTECT, db_index=True)
+
+class order(models.Model):
+    datetime = models.DateTimeField(auto_now_add=True)
+    worker = models.ForeignKey(worker, on_delete=models.PROTECT)
+    customer = models.ForeignKey(customer, on_delete=models.PROTECT)
